c68d85b299b96db9d70341eabaceaeefeac8482b
vishal <vkumars426675@gmail.com>
2024-11-21 23:15:06 +0530

    first commit

--- src/Components/Course.jsx
+++ src/Components/Course.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Card from './card'
+import Card from './Card'
 import { Link } from 'react-router-dom'
 import axios from "axios";
 function Course() {
